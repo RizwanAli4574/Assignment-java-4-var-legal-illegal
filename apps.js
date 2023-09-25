496ccbbe4beb1d4a7cb4f4fbc9cdeb8f84f5c559
RizwanAli4574 <137984475+RizwanAli4574@users.noreply.github.com>
2023-09-25 18:53:16 +0500

    Assignment-java-4-var-legal-illegal

---
+++ apps.js
@@ -0,0 +1,42 @@
+var variable1 = " Hello, World!\n",
+    variable2 = "Testing...\n",
+    variable3 =  42;
+
+    console.log(variable1,variable2,variable3)
+
+
+    var one$ = " legal variabel 1 \n"
+    var two_ = "legal variabel 2\n"
+    var three = "legal variabel 3\n"
+    var four = "legal variabel 4\n"
+    var five = "legal variabel 5\n"
+
+    console.log(one$,two_,three,four,five)
+
+    var Rules1 ="A heading stating “Rules for naming JS variables"
+    document.write(Rules1)
+    document.write("<br>")
+
+    var Rules2 ="Variable names can only contain letters, numbers, dollar signs, and underscores.For example $my_1stVariable"
+    document.write(Rules2)
+    document.write("<br>")
+
+    var Rules3 ="Variables must begin with a letters, $ or underscores. For example $name, _name or name"
+    document.write(Rules3)
+    document.write("<br>")
+
+    var Rules4 ="Variable names are case sensitive"
+    document.write(Rules4)
+    document.write("<br>")
+
+    var Rules5 ="Variable names should not be JS keywords"
+    document.write(Rules5)
+
+    
+                   
+    
+    
+   
+   
+   
+   
